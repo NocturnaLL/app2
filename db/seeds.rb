cc2b83aec3441d0cd1a531a6cb7c91e6d806eff2
Burak <bfidans1@binghamton.edu>
2016-08-23 17:56:46 +0300

    Seeds.rb added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Note.create(name:"Almanca", info:"wenig: dar, allein: yalnız, leiten,tut : yapmak, wichting: önemli ")
+Note.create(name:"Almanca", info:"aufstehen: kalkmak")
