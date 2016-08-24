dabf5f050db1421236f4b0dc42e6c81f9fe19f04
Burak <bfidans1@binghamton.edu>
2016-08-24 20:06:17 +0300

    render form added, new note controller added, routes resources removed

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,9 +1,27 @@
 class NotesController < ApplicationController
+	def new
+		@note = Note.new
+	end
+
 	def index
 		@notes = Note.all
 	end	
 
 	def show 
 		@note = Note.find(params[:id])
+	end
+
+	def create
+		@note = Note.new(strong_params)
+
+		if @note.save
+			redirect_to notes_path(@note)
+		else
+			render :new
+		end
+	end
+
+	def strong_params
+		params.require(:note).permit(:name, :info)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
-  resources :notes
+  #resources :notes
   resources :home, only: [:index]
+  get 'new/note', to: 'notes#new'
+  get 'notes', to: 'notes#index'
+  get 'notes/:id', to: 'notes#show', as: "note"
+  post 'notes', to: 'notes#create'
 end
